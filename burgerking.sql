c9373e49f55ce55bab374eb08c18952a69bb67f3
Sumit Baviskar <153518735+Sumit-Baviskar@users.noreply.github.com>
2025-02-10 22:22:34 +0530

    Update burgerking.sql

--- burgerking.sql
+++ burgerking.sql
@@ -18,8 +18,11 @@ CREATE TABLE burger_king_menu (
     dietary_fiber_g DECIMAL(5,2) NOT NULL,
     sugars_g DECIMAL(5,2) NOT NULL,
     protein_g DECIMAL(5,2) NOT NULL);
-    
+
+
+
 SELECT * FROM burger_king_menu;
+
 
 Describe burger_king_menu;
 
@@ -32,6 +35,7 @@ FROM burger_king_menu
 GROUP BY category
 ORDER BY percentage DESC;
 
+
 -- Most Nutritious Item per Category (Highest Protein)
 
 SELECT X.*
@@ -42,12 +46,13 @@ FROM (
 WHERE X.rn = 1;
 
 
-
 -- Top 10 items with the highest calories
 
 SELECT item FROM burger_king_menu
 ORDER BY calories DESC
 LIMIT 10;
+
+
 
 -- Top 10 items with the highest fat content
 
@@ -63,6 +68,7 @@ FROM burger_king_menu
 ORDER BY 'Trans Fat (g)' DESC
 LIMIT 10;
 
+
 --  Top 10 Highest Dietary Fiber Items
 
 SELECT *
@@ -70,7 +76,8 @@ FROM burger_king_menu
 ORDER BY 'Dietary Fiber (g)' DESC
 LIMIT 10;
 
-select * from burger_king_menu;
+
+
 -- Healthy Index Score (Custom Formula)
 SELECT item, 
        (protein_g * 2 + dietary_fiber_g * 1.5 - fat_g - sugars_g) AS health_score 
@@ -78,14 +85,7 @@ FROM burger_king_menu
 ORDER BY health_score DESC
 LIMIT 10;
 
--- Most Nutritious Item Per Category (Highest Protein)
-SELECT X.* 
-FROM (
-    SELECT *, 
-           DENSE_RANK() OVER(PARTITION BY category ORDER BY protein_g DESC) AS rn 
-    FROM burger_king_menu
-) X
-WHERE X.rn = 1;
+
 
 -- Finding the Most Balanced Meal (Equal Fat, Protein, and Carbs)
 
@@ -95,6 +95,7 @@ FROM burger_king_menu
 ORDER BY balance_score ASC
 LIMIT 10;
 
+
 -- High-Protein, Low-Carb Options
 
 SELECT item, protein_g, total_carb_g 
@@ -102,6 +103,7 @@ FROM burger_king_menu
 WHERE total_carb_g < 20  
 ORDER BY protein_g DESC
 LIMIT 10;
+
 
 -- Salt-to-Fat Ratio
 
@@ -118,6 +120,8 @@ FROM burger_king_menu
 ORDER BY fat_cal_ratio DESC
 LIMIT 10;
 
+
+    
 -- Least Healthy Items (Highest Calories & Fat)
 
 SELECT item, calories, fat_g 
@@ -125,9 +129,11 @@ FROM burger_king_menu
 ORDER BY calories DESC, fat_g DESC
 LIMIT 10;
 
+
 -- Lowest Sodium (Healthier Choices for Heart Health)
 
 SELECT item, sodium_mg 
 FROM burger_king_menu
 ORDER BY sodium_mg ASC
 LIMIT 10;
+
